fc5f5924c980a7e90c8b29c02363b3c533715343
VirtualPirate <artaza13020artaza@gmail.com>
2023-06-04 13:31:49 +0530

    This is Fist commit of insta-dl npm package

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export async function getReelInfo(url: string) {
+  // Get the raw HTML
+  const rawHTML = (await axios.get(url)).data;
+
+  // calls cheerio to process the html received
+  const $ = cheerio.load(rawHTML);
+
+  // Extracts the Utf-8 encoded json object from HTML
+  const rawInfo = JSON.parse(
+    $('script[type="application/ld+json"]').text().toString()
+  );
+
+  // Refines the rawInfo extracted
+  const refineInfo = {
+    author: {
+      id: rawInfo?.author?.identifier?.value,
+      image: rawInfo?.author?.image,
+      name: rawInfo?.author?.name,
+    },
+
+    video: {
+      likesCount: rawInfo?.interactionStatistic?.find((item: any) => {
+        return item.interactionType.includes("LikeAction");
+      }).userInteractionCount,
+
+      commentCount: rawInfo?.interactionStatistic?.find((item: any) => {
+        return item.interactionType.includes("CommentAction");
+      }).userInteractionCount,
+
+      ...rawInfo?.video[0],
+    },
+  };
+
+  return refineInfo;
+}
